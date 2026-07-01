51c9cfa9ff2a589971debb2777f9d4f0f05afec3
inakiUnzaga <inakiunzaga88@gmail.com>
2026-07-01 16:48:56 -0300

    Revert "Integrando mis cambios con los del servidor"
    This reverts commit 612b4648169036b0cd6b7ba6b482d07ac964ca50.

--- HogarYa/src/main/java/com/hogarya/entidades/Propiedad.java
+++ HogarYa/src/main/java/com/hogarya/entidades/Propiedad.java
@@ -3,7 +3,6 @@ package com.hogarya.entidades;
 import com.hogarya.entidades.enums.EstadoDisponibilidad;
 import com.hogarya.entidades.enums.TipoPropiedad;
 import jakarta.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,7 +22,6 @@ public class Propiedad {
     private Double metrosCuadrados;
     private String descripcion;
     private String comodidades;
-    private Double precio;
 
     @Enumerated(EnumType.STRING)
     private EstadoDisponibilidad estadoDisponibilidad;
@@ -39,45 +37,124 @@ public class Propiedad {
     private Persona propietario;
 
     @OneToMany(mappedBy = "propiedad")
-    private List<HistorialEstadoPropiedad> historialEstados = new ArrayList<>();
+    private List<HistorialEstadoPropiedad> historialEstados;
 
     @OneToMany(mappedBy = "propiedad")
-    private List<Publicacion> publicaciones = new ArrayList<>();
+    private List<Publicacion> publicaciones;
 
     @OneToMany(mappedBy = "propiedad")
-    private List<Contrato> contratos = new ArrayList<>();
+    private List<Contrato> contratos;
 
-    public Propiedad() {
+
+    public Long getId() {
+        return id;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getDireccion() { return direccion; }
-    public void setDireccion(String direccion) { this.direccion = direccion; }
-    public TipoPropiedad getTipo() { return tipo; }
-    public void setTipo(TipoPropiedad tipo) { this.tipo = tipo; }
-    public Integer getCantidadAmbientes() { return cantidadAmbientes; }
-    public void setCantidadAmbientes(Integer cantidadAmbientes) { this.cantidadAmbientes = cantidadAmbientes; }
-    public Double getMetrosCuadrados() { return metrosCuadrados; }
-    public void setMetrosCuadrados(Double metrosCuadrados) { this.metrosCuadrados = metrosCuadrados; }
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public String getComodidades() { return comodidades; }
-    public void setComodidades(String comodidades) { this.comodidades = comodidades; }
-    public EstadoDisponibilidad getEstadoDisponibilidad() { return estadoDisponibilidad; }
-    public void setEstadoDisponibilidad(EstadoDisponibilidad estadoDisponibilidad) { this.estadoDisponibilidad = estadoDisponibilidad; }
-    public boolean isEliminada() { return eliminada; }
-    public void setEliminada(boolean eliminada) { this.eliminada = eliminada; }
-    public Ciudad getCiudad() { return ciudad; }
-    public void setCiudad(Ciudad ciudad) { this.ciudad = ciudad; }
-    public Persona getPropietario() { return propietario; }
-    public void setPropietario(Persona propietario) { this.propietario = propietario; }
-    public List<HistorialEstadoPropiedad> getHistorialEstados() { return historialEstados; }
-    public void setHistorialEstados(List<HistorialEstadoPropiedad> historialEstados) { this.historialEstados = historialEstados; }
-    public List<Publicacion> getPublicaciones() { return publicaciones; }
-    public void setPublicaciones(List<Publicacion> publicaciones) { this.publicaciones = publicaciones; }
-    public List<Contrato> getContratos() { return contratos; }
-    public void setContratos(List<Contrato> contratos) { this.contratos = contratos; }
-    public Double getPrecio() { return precio; }
-    public void setPrecio(Double precio) { this.precio = precio; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public TipoPropiedad getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPropiedad tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getCantidadAmbientes() {
+        return cantidadAmbientes;
+    }
+
+    public void setCantidadAmbientes(Integer cantidadAmbientes) {
+        this.cantidadAmbientes = cantidadAmbientes;
+    }
+
+    public Double getMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void setMetrosCuadrados(Double metrosCuadrados) {
+        this.metrosCuadrados = metrosCuadrados;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getComodidades() {
+        return comodidades;
+    }
+
+    public void setComodidades(String comodidades) {
+        this.comodidades = comodidades;
+    }
+
+    public EstadoDisponibilidad getEstadoDisponibilidad() {
+        return estadoDisponibilidad;
+    }
+
+    public void setEstadoDisponibilidad(EstadoDisponibilidad estadoDisponibilidad) {
+        this.estadoDisponibilidad = estadoDisponibilidad;
+    }
+
+    public boolean isEliminada() {
+        return eliminada;
+    }
+
+    public void setEliminada(boolean eliminada) {
+        this.eliminada = eliminada;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Persona getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Persona propietario) {
+        this.propietario = propietario;
+    }
+
+    public List<HistorialEstadoPropiedad> getHistorialEstados() {
+        return historialEstados;
+    }
+
+    public void setHistorialEstados(List<HistorialEstadoPropiedad> historialEstados) {
+        this.historialEstados = historialEstados;
+    }
+
+    public List<Publicacion> getPublicaciones() {
+        return publicaciones;
+    }
+
+    public void setPublicaciones(List<Publicacion> publicaciones) {
+        this.publicaciones = publicaciones;
+    }
+
+    public List<Contrato> getContratos() {
+        return contratos;
+    }
+
+    public void setContratos(List<Contrato> contratos) {
+        this.contratos = contratos;
+    }
 }
